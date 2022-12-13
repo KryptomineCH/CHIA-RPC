7fb2836279d7b423ea31554b7db257e23e58cc2d
Julian B <bechti44@googlemail.com>
2022-12-13 20:50:44 +0100

    fixed missing tostring override

--- CHIA-RPC/Wallet_RPC_NS/KeyManagement/CheckDeleteKey_RPC.cs
+++ CHIA-RPC/Wallet_RPC_NS/KeyManagement/CheckDeleteKey_RPC.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CHIA_RPC.Wallet_RPC_NS.Wallet;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text;
 
 namespace CHIA_RPC.Wallet_RPC_NS.KeyManagement
 {
@@ -36,5 +39,46 @@ namespace CHIA_RPC.Wallet_RPC_NS.KeyManagement
         /// </summary>
         /// <remarks>optional</remarks>
         public ulong max_ph_to_search { get; set; }
+        /// <summary>
+        /// saves the rpc as rpc-file (json) to the specified path
+        /// </summary>
+        /// <param name="path"></param>
+        public void Save(string path)
+        {
+            if (!path.EndsWith(".rpc"))
+            {
+                path += ".rpc";
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.WriteIndented = true;
+            options.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+            string testText = JsonSerializer.Serialize(this, options: options);
+            Encoding utf8WithoutBom = new UTF8Encoding(false); // no bom
+            File.WriteAllText(path, testText, utf8WithoutBom);
+        }
+        /// <summary>
+        /// loads an rpc file from the specified path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static SendXCH_RPC Load(string path)
+        {
+            FileInfo testFile = new FileInfo(path);
+            string text = File.ReadAllText(testFile.FullName);
+            SendXCH_RPC rpc = JsonSerializer.Deserialize<SendXCH_RPC>(text);
+            return rpc;
+        }
+        /// <summary>
+        /// serializes this object into a json string
+        /// </summary>
+        /// <returns>json formatted string</returns>
+        public override string ToString()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.WriteIndented = false;
+            options.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+            string jsonString = JsonSerializer.Serialize(this, options: options);
+            return jsonString;
+        }
     }
 }
