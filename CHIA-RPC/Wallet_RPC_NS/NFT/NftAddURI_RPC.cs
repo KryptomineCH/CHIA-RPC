0141b442fd8e6ff8a19047af2732f801cb649b7f
Julian B <bechti44@googlemail.com>
2023-01-09 23:00:15 +0100

    fixed reference error

--- CHIA-RPC/Wallet_RPC_NS/NFT/NftAddURI_RPC.cs
+++ CHIA-RPC/Wallet_RPC_NS/NFT/NftAddURI_RPC.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CHIA_RPC.Wallet_RPC_NS.NFT
+﻿namespace CHIA_RPC.Wallet_RPC_NS.NFT
 {
     public class NftAddURI_Response
     {
