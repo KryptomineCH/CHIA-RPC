a3e987c62c92fd088f0b4dfa63ad043077fbd684
Julian B <bechti44@googlemail.com>
2023-01-13 16:22:13 +0100

    -  added did_get_did support

---
+++ CHIA-RPC/Wallet_RPC_NS/DID_NS/DidGetDid_Response.cs
@@ -0,0 +1,14 @@
+﻿namespace CHIA_RPC.Wallet_RPC_NS.DID_NS
+{
+    public class DidGetDid_Response
+    {
+        public string coin_id { get; set; }
+        /// <summary>
+        /// the did_id of the wallet
+        /// </summary>
+        public string my_did { get; set; }
+        public ulong wallet_id { get; set; }
+        public bool success { get; set; }
+        public string error { get; set; }
+    }
+}
