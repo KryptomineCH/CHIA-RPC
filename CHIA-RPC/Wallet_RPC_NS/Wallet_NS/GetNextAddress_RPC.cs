2d265576500b30d9873cba90221a2cbbf05ab11b
Julian B <bechti44@googlemail.com>
2023-01-14 23:38:12 +0100

    added better xaml documentation for getnextadress

--- CHIA-RPC/Wallet_RPC_NS/Wallet_NS/GetNextAddress_RPC.cs
+++ CHIA-RPC/Wallet_RPC_NS/Wallet_NS/GetNextAddress_RPC.cs
@@ -5,13 +5,29 @@ namespace CHIA_RPC.Wallet_RPC_NS.Wallet_NS
 {
     public class GetNextAddress_Response
     {
+        /// <summary>
+        /// chia adress, eg <br/>
+        /// xch1jxe8yfq8xem0e8ty5dq9hp86ududys96n6ugy2q4fextr0pplw9qwpfru9
+        /// </summary>
         public string address { get; set; }
-        public bool success { get; set; }
+        /// <summary>
+        /// the wallet number which this request was made for (eg. 2)
+        /// </summary>
         public ulong wallet_id { get; set; }
+        /// <summary>
+        /// wether the request against the chia daemon was sucessful or not
+        /// </summary>
+        public bool success { get; set; }
+        /// <summary>
+        /// if there was an error, this will contain the error message
+        /// </summary>
         public string error { get; set; }
     }
     public class GetNextAddress_RPC
     {
+        /// <summary>
+        /// the wallet number of the wallet in the chia client (eg. 2)
+        /// </summary>
         public ulong wallet_id { get; set; }
         /// <summary>
         /// specifies if a new address should be generated or the last one used
