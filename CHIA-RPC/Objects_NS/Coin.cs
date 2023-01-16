9cbd831286696a58005e9d390273431a7e050c0e
Julian B <bechti44@googlemail.com>
2023-01-16 22:48:31 +0100

    fixed coin

--- CHIA-RPC/Objects_NS/Coin.cs
+++ CHIA-RPC/Objects_NS/Coin.cs
@@ -10,11 +10,11 @@ namespace CHIA_RPC.Objects_NS
         public ulong amount { get; set; }
         public string parent_coin_info { get; set; }
         public string puzzle_hash { get; set; }
-        public string GeneratoCoinID()
+        public string GetCoinID()
         {
             // convert values to bytes
-            byte[] parent_id_bytes = Convert.FromHexString(parent_coin_info);
-            byte[] puzzle_hash_bytes = Convert.FromHexString(puzzle_hash);
+            byte[] parent_id_bytes = Convert.FromHexString(parent_coin_info[2..]);
+            byte[] puzzle_hash_bytes = Convert.FromHexString(puzzle_hash[2..]);
             var amount_hex_string = amount.ToString("X");
             // add leading zero if needed
             amount_hex_string = (amount_hex_string.Length % 2 == 0 ? "" : "0") + amount_hex_string;
@@ -25,7 +25,7 @@ namespace CHIA_RPC.Objects_NS
 
             var hash = SHA256.Create();
             byte[] coin_id_bytes = hash.ComputeHash(bytes);
-            string coin_id = "0x"+Convert.ToHexString(coin_id_bytes);
+            string coin_id = "0x"+Convert.ToHexString(coin_id_bytes).ToLower();
             return coin_id;
         }
     }
