966288a34db9e2536fce55f64b04875ae08e4f3d
Julian B <bechti44@googlemail.com>
2023-01-16 21:52:16 +0100

    added capability to generate coin id from coin object

--- CHIA-RPC/Objects_NS/Coin.cs
+++ CHIA-RPC/Objects_NS/Coin.cs
@@ -1,4 +1,6 @@
-﻿namespace CHIA_RPC.Objects_NS
+﻿using System.Security.Cryptography;
+
+namespace CHIA_RPC.Objects_NS
 {
     public class Coin
     {
@@ -8,5 +10,23 @@
         public ulong amount { get; set; }
         public string parent_coin_info { get; set; }
         public string puzzle_hash { get; set; }
+        public string GeneratoCoinID()
+        {
+            // convert values to bytes
+            byte[] parent_id_bytes = Convert.FromHexString(parent_coin_info);
+            byte[] puzzle_hash_bytes = Convert.FromHexString(puzzle_hash);
+            var amount_hex_string = amount.ToString("X");
+            // add leading zero if needed
+            amount_hex_string = (amount_hex_string.Length % 2 == 0 ? "" : "0") + amount_hex_string;
+            byte[] amount_bytes = Convert.FromHexString(amount_hex_string);
+
+            // concat all to one bytes array
+            byte[] bytes = parent_id_bytes.Concat(puzzle_hash_bytes).Concat(amount_bytes).ToArray();
+
+            var hash = SHA256.Create();
+            byte[] coin_id_bytes = hash.ComputeHash(bytes);
+            string coin_id = "0x"+Convert.ToHexString(coin_id_bytes);
+            return coin_id;
+        }
     }
 }
