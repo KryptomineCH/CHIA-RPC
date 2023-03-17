56d0dc69051faff4eeb2014672e2bb672bd14c24
Julian B <bechti44@googlemail.com>
2023-03-17 09:13:14 +0100

    added new functions from documentation

---
+++ CHIA-RPC/Wallet_RPC_NS/WalletNode_NS/PushTx_RPC.cs
@@ -0,0 +1,19 @@
+﻿using CHIA_RPC.Objects_NS;
+using CHIA_RPC.HelperFunctions_NS;
+
+namespace CHIA_RPC.Wallet_RPC_NS.WalletNode_NS
+{
+    /// <summary>
+    /// Push a spend bundle (transaction) to the blockchain
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://docs.chia.net/wallet-rpc#push_tx"/>
+    /// </remarks>
+    public class PushTx_RPC : RPCTemplate<PushTx_RPC>
+    {
+        /// <summary>
+        /// The spend bundle (transaction) to push
+        /// </summary>
+        public SpendBundle spend_bundle { get; set; }
+    }
+}
---
+++ CHIA-RPC/Wallet_RPC_NS/WalletNode_NS/SetWalletResyncOnStartup_RPC.cs
@@ -0,0 +1,20 @@
+﻿using CHIA_RPC.HelperFunctions_NS;
+using static System.Net.WebRequestMethods;
+
+namespace CHIA_RPC.Wallet_RPC_NS.WalletNode_NS
+{
+    /// <summary>
+    /// Represents a JSON RPC request for the "set_wallet_resync_on_startup" command in the Chia wallet.
+    /// </summary>
+    /// <remarks>
+    /// returns a Success_Response <br/>
+    /// <see href="https://docs.chia.net/wallet-rpc#set_wallet_resync_on_startup"/>
+    /// </remarks>
+    public class SetWalletResyncOnStartup_RPC : RPCTemplate<SetWalletResyncOnStartup_RPC>
+    {
+        /// <summary>
+        /// Set to true to enable resync. Defaults to true if not provided.
+        /// </summary>
+        public bool? enable { get; set; }
+    }
+}
