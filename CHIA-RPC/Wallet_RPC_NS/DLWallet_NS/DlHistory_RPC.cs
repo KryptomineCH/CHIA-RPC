df746aebb0ee68262673de4877fea48a79196bac
Julian B <bechti44@googlemail.com>
2023-03-24 20:49:21 +0100

    implemented datalayer wallet

---
+++ CHIA-RPC/Wallet_RPC_NS/DLWallet_NS/DlHistory_RPC.cs
@@ -0,0 +1,55 @@
+﻿using CHIA_RPC.HelperFunctions_NS;
+using CHIA_RPC.Wallet_RPC_NS.DLWallet_NS.DLWalletObjects_NS;
+
+namespace CHIA_RPC.Wallet_RPC_NS.DLWallet_NS
+{
+    /// <summary>
+    /// Class for deserializing the response of the "dl_history" RPC request in the Chia blockchain.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://docs.chia.net/wallet-rpc#dl_history"/><br/><br/>
+    /// Using:<br/><see cref="DlHistory_RPC"/>
+    /// </remarks>
+    public class DlHistory_Response : ResponseTemplate<DlHistory_Response>
+    {
+        /// <summary>
+        /// The number of records returned.
+        /// </summary>
+        public ulong? count { get; set; }
+
+        /// <summary>
+        /// An array of the records returned.
+        /// </summary>
+        public HistoryRecord[] history { get; set; }
+    }
+
+    /// <summary>
+    /// RPC request class for the "dl_history" command in the Chia wallet RPC API.
+    /// Show the history of a data store
+    /// </summary>
+    /// <remarks><see href="https://docs.chia.net/wallet-rpc#dl_history"/></remarks>
+    /// <returns><see cref="DlHistory_Response"/></returns>
+    public class DlHistory_RPC : RPCTemplate<DlHistory_RPC>
+    {
+        /// <summary>
+        /// The launcher ID of the DataLayer wallet.
+        /// </summary>
+        public string launcher_id { get; set; }
+
+        /// <summary>
+        /// The first generation of singleton to show [Default: none].
+        /// </summary>
+        public ulong? min_generation { get; set; }
+
+        /// <summary>
+        /// The last generation of the singleton to show [Default: none].
+        /// </summary>
+        public ulong? max_generation { get; set; }
+
+        /// <summary>
+        /// The number of results to show [Default: show all results].
+        /// </summary>
+        public ulong? num_results { get; set; }
+    }
+
+}
---
+++ CHIA-RPC/Wallet_RPC_NS/DLWallet_NS/DlNewMirror_RPC.cs
@@ -0,0 +1,48 @@
+﻿using CHIA_RPC.HelperFunctions_NS;
+using CHIA_RPC.Objects_NS;
+
+namespace CHIA_RPC.Wallet_RPC_NS.DLWallet_NS
+{
+    /// <summary>
+    /// Represents the response for the dl_new_mirror command.
+    /// This command adds a new on chain message for a specific singleton.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://docs.chia.net/wallet-rpc#dl_new_mirror"/><br/><br/>
+    /// Uses:<br/><see cref="DlNewMirror_RPC"/>
+    /// </remarks>
+    public class DlNewMirror_Response : ResponseTemplate<DlNewMirror_Response>
+    {
+        /// <summary>
+        /// An array of transactions returned in the response.
+        /// </summary>
+        public Transaction[] transactions { get; set; }
+    }
+    /// <summary>
+    /// Add a new on chain message for a specific singleton
+    /// </summary>
+    /// <remarks><see href="https://docs.chia.net/wallet-rpc#dl_new_mirror"/></remarks>
+    /// <returns><see cref="DlNewMirror_Response"/></returns>
+    public class DlNewMirror_RPC : RPCTemplate<DlNewMirror_RPC>
+    {
+        /// <summary>
+        /// The launcher ID of the DataLayer wallet
+        /// </summary>
+        public string launcher_id { get; init; }
+
+        /// <summary>
+        /// A list of URLs to be used for the mirror
+        /// </summary>
+        public string[] urls { get; init; }
+
+        /// <summary>
+        /// The value of the mirror (in mojos) to spend to create the mirror. In theory, mirrors with a higher amount will be prioritized
+        /// </summary>
+        public ulong amount { get; init; }
+
+        /// <summary>
+        /// An optional blockchain fee, in mojos
+        /// </summary>
+        public ulong? fee { get; init; }
+    }
+}
