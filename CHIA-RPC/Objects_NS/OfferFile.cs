b96b56812e1e159d328412bf08c2829393d58d4f
Julian B <bechti44@googlemail.com>
2023-03-24 16:59:45 +0100

    implemented wallet_catsandtrading

--- CHIA-RPC/Objects_NS/OfferFile.cs
+++ CHIA-RPC/Objects_NS/OfferFile.cs
@@ -24,4 +24,25 @@ namespace CHIA_RPC.Objects_NS
             File.WriteAllText(path, offer, utf8WithoutBom);
         }
     }
+    public class OfferFiles : ObjectTemplate<OfferFiles>
+    {
+        public string[] offers { get; set; }
+        public TradeRecord[] trade_records { get; set; }
+
+        public bool success { get; set; }
+        public string? error { get; set; }
+        /// <summary>
+        /// exports the offer as offer file which can be uploaded to exchanges
+        /// </summary>
+        /// <param name="path"></param>
+        public void Export(string path)
+        {
+            if (!path.EndsWith(".offer"))
+            {
+                path += ".offer";
+            }
+            Encoding utf8WithoutBom = new UTF8Encoding(false); // no bom
+            File.WriteAllText(path, offer, utf8WithoutBom);
+        }
+    }
 }
---
+++ CHIA-RPC/Wallet_RPC_NS/CATsAndTrading_NS/CatAssetIdToName_RPC.cs
@@ -0,0 +1,39 @@
+﻿
+
+using CHIA_RPC.HelperFunctions_NS;
+
+namespace CHIA_RPC.Wallet_RPC_NS.CATsAndTrading_NS
+{
+    /// <summary>
+    /// Represents a response for retrieving a CAT's name from its ID.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://docs.chia.net/wallet-rpc/#cat_asset_id_to_name"/><br/><br/>
+    /// Uses:<br/><see cref="CatAssetIdToName_RPC"/>
+    /// </remarks>
+    public class CatAssetIdToName_Response : ResponseTemplate<CatAssetIdToName_Response>
+    {
+        /// <summary>
+        /// The name of the retrieved CAT.
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// The ID of the wallet whose name was changed.
+        /// </summary>
+        public ulong? wallet_id { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a request to retrieve a CAT's name from its ID.
+    /// </summary>
+    /// <remarks><see href="https://docs.chia.net/wallet-rpc/#cat_asset_id_to_name"/></remarks>
+    /// <returns><see cref="CatAssetIdToName_Response"/></returns>
+    public class CatAssetIdToName_RPC : RPCTemplate<CatAssetIdToName_RPC>
+    {
+        /// <summary>
+        /// The ID of the CAT whose name you would like to retrieve. This CAT must be listed in your DEFAULT_CATS, ie the CATs your wallet recognizes.
+        /// </summary>
+        public string asset_id { get; set; }
+    }
+}
