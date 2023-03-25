c56b240cb28a47eec6c32eea247081d5021f931e
Julian B <bechti44@googlemail.com>
2023-03-25 22:55:33 +0100

    updated memos with comment

--- CHIA-RPC/Objects_NS/Memos.cs
+++ CHIA-RPC/Objects_NS/Memos.cs
@@ -2,6 +2,9 @@
 
 namespace CHIA_RPC.Objects_NS
 {
+    /// <summary>
+    /// a memo is of type string and might be attached to a <see cref="Transaction"/>
+    /// </summary>
     public class Memos : ObjectTemplate<Memos>
     {
         public string memo { get; set; }
