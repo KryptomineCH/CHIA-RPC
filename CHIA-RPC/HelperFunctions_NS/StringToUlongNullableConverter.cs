685fa254e1e1443f099cc0fd0a54166517a70d73
Julian B <bechti44@googlemail.com>
2023-03-25 22:54:52 +0100

    added string to ulong custom json converter

---
+++ CHIA-RPC/HelperFunctions_NS/StringToUlongNullableConverter.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace CHIA_RPC.HelperFunctions_NS
+{
+    public class StringToUlongNullableConverter : JsonConverter<ulong?>
+    {
+        public override ulong? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                string stringValue = reader.GetString();
+                if (ulong.TryParse(stringValue, out ulong ulongValue))
+                {
+                    return ulongValue;
+                }
+                return null;
+            }
+            else if (reader.TokenType == JsonTokenType.Number)
+            {
+                return reader.GetUInt64();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, ulong? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(value.Value.ToString());
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+        }
+    }
+
+}
