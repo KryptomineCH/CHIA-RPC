9b99c05566235d38deadb5afd4ab09dd317397c3
Julian B <bechti44@googlemail.com>
2023-03-25 21:20:15 +0100

    implemented routes and connections

---
+++ CHIA-RPC/Wallet_RPC_NS/RoutesAndConnections_NS/CloseConnection_RPC.cs
@@ -0,0 +1,18 @@
+﻿using CHIA_RPC.HelperFunctions_NS;
+
+namespace CHIA_RPC.Wallet_RPC_NS.RoutesAndConnections_NS
+{
+    /// <summary>
+    /// Represents a JSON-RPC request for closing an active connection to a node.
+    /// </summary>
+    /// <remarks><see href="https://docs.chia.net/wallet-rpc/#close_connection"/></remarks>
+    /// <returns><see cref="General_NS.Success_Response"/></returns>
+    public class CloseConnection_RPC : RPCTemplate<CloseConnection_RPC>
+    {
+        /// <summary>
+        /// The hex ID of the node to close, obtainable from get_connections
+        /// </summary>
+        public string node_id { get; set; }
+    }
+
+}
---
+++ CHIA-RPC/Wallet_RPC_NS/RoutesAndConnections_NS/OpenConnection_RPC.cs
@@ -0,0 +1,23 @@
+﻿using CHIA_RPC.HelperFunctions_NS;
+
+namespace CHIA_RPC.Wallet_RPC_NS.RoutesAndConnections_NS
+{
+    /// <summary>
+    /// RPC request to open a connection to another node.
+    /// </summary>
+    /// <remarks><see href="https://docs.chia.net/wallet-rpc/#open_connection"/></remarks>
+    /// <returns><see cref="General_NS.Success_Response"/></returns>
+    public class OpenConnection_RPC : RPCTemplate<OpenConnection_RPC>
+    {
+        /// <summary>
+        /// The IP or URL of the node to connect to.
+        /// </summary>
+        public string host { get; set; }
+
+        /// <summary>
+        /// The port through which to connect to the remote node.
+        /// </summary>
+        public ulong port { get; set; }
+    }
+
+}
