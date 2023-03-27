953ebfcd3dd9a64b4ea2425df499a0152886f5f6
Julian B <bechti44@googlemail.com>
2023-03-27 15:26:41 +0200

    added boolean converter

---
+++ CHIA-RPC/HelperFunctions_NS/StringToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace CHIA_RPC.HelperFunctions_NS
+{
+    public class StringToBoolConverter : JsonConverter<bool>
+    {
+        public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string stringValue = reader.GetString();
+            return bool.TryParse(stringValue, out var result) ? result : throw new JsonException($"Invalid value '{stringValue}' for boolean.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString().ToLower());
+        }
+    }
+
+}
