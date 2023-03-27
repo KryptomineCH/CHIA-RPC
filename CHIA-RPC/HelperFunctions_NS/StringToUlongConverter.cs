e1fc3b1eef097f01a0aaa06a66782a338dac15c2
Julian B <bechti44@googlemail.com>
2023-03-27 15:26:30 +0200

    added ulong converter

--- CHIA-RPC/HelperFunctions_NS/StringToUlongConverter.cs
+++ CHIA-RPC/HelperFunctions_NS/StringToUlongConverter.cs
@@ -3,6 +3,34 @@ using System.Text.Json;
 
 namespace CHIA_RPC.HelperFunctions_NS
 {
+    public class StringToUlongConverter : JsonConverter<ulong>
+    {
+        public override ulong Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                string stringValue = reader.GetString();
+                if (ulong.TryParse(stringValue, out ulong ulongValue))
+                {
+                    return ulongValue;
+                }
+                return 0;
+            }
+            else if (reader.TokenType == JsonTokenType.Number)
+            {
+                return reader.GetUInt64();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, ulong value, JsonSerializerOptions options)
+        {
+                writer.WriteStringValue(value.ToString());
+        }
+    }
     public class StringToUlongNullableConverter : JsonConverter<ulong?>
     {
         public override ulong? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
