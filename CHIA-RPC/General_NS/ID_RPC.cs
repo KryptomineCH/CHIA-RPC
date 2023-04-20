b5d2099dd9341f583f9043bc5a4d56e1dbed479b
Julian B <bechti44@googlemail.com>
2023-04-20 10:40:58 +0200

    added constructor with parameters

--- CHIA-RPC/General_NS/ID_RPC.cs
+++ CHIA-RPC/General_NS/ID_RPC.cs
@@ -4,6 +4,13 @@ namespace CHIA_RPC.General_NS
 {
     public class ID_RPC : RPCTemplate<ID_RPC>
     {
+        public ID_RPC() { /* required for serializer */ }
+        public ID_RPC(string id, ulong? fee = null)
+        {
+            this.id = id;
+            this.fee = fee;
+        }
+
         public string id { get; set; }
         /// <summary>
         /// note: this value may only be used for requests which trigger a transaction on the blockchain
