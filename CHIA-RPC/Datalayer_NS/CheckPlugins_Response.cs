f65bab208c60c79b6264af9b94b8a49ad54b4dd4
Julian B <bechti44@googlemail.com>
2023-05-11 20:57:46 +0200

    added Datalayer check plugins for 1.8

---
+++ CHIA-RPC/Datalayer_NS/CheckPlugins_Response.cs
@@ -0,0 +1,19 @@
+﻿using CHIA_RPC.HelperFunctions_NS;
+
+namespace CHIA_RPC.Datalayer_NS
+{
+    /// <summary>
+    /// Get information about configured uploader/downloader plugins
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://docs.chia.net/datalayer-rpc#check_plugins"/><br/><br/>
+    /// Uses:<br/><see cref="GetKeys_RPC"/>
+    /// </remarks>
+    public class CheckPlugins_Response : ResponseTemplate<CheckPlugins_Response>
+    {
+        /// <summary>
+        /// unknown due to lacking documentation
+        /// </summary>
+        public dynamic plugin_status { get; set; }
+    }
+}
