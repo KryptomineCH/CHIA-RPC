0beb5fec1462d9edd6a17a212d27552f22e9e966
Julian B <bechti44@googlemail.com>
2023-05-11 10:57:57 +0200

    added conversion functionality from WalletID RPC & Response

--- CHIA-RPC/Wallet_NS/Wallet_NS/GetNextAddress_RPC.cs
+++ CHIA-RPC/Wallet_NS/Wallet_NS/GetNextAddress_RPC.cs
@@ -1,4 +1,5 @@
-﻿using CHIA_RPC.HelperFunctions_NS;
+﻿using CHIA_RPC.General_NS;
+using CHIA_RPC.HelperFunctions_NS;
 
 namespace CHIA_RPC.Wallet_NS.Wallet_NS
 {
@@ -29,12 +30,46 @@ namespace CHIA_RPC.Wallet_NS.Wallet_NS
     public class GetNextAddress_RPC : RPCTemplate<GetNextAddress_RPC>
     {
         /// <summary>
+        /// this constructor is parameterless, primairly used for serialisation purposes
+        /// </summary>
+        public GetNextAddress_RPC() { /* required for serialisation */ }
+        /// <summary>
+        /// generates the request for retrieving a wallets adress
+        /// </summary>
+        /// <param name="walletID">the sub wallet which to retrieve an address from</param>
+        /// <param name="generateNewAddress">specifies if a new random address should be generated for privacy purposes</param>
+        public GetNextAddress_RPC(ulong walletID, bool generateNewAddress = false)
+        {
+            this.wallet_id = walletID;
+            this.new_address = generateNewAddress;
+        }
+        /// <summary>
+        /// derives the request for retrieving a wallets adress from a wallet id rpc
+        /// </summary>
+        /// <param name="derivation">the wallet id rpc which to derive the request from</param>
+        /// <param name="generateNewAddress">specifies if a new random address should be generated for privacy purposes</param>
+        public GetNextAddress_RPC(WalletID_RPC derivation, bool generateNewAddress = false)
+        {
+            this.wallet_id = derivation.wallet_id;
+            this.new_address = generateNewAddress;
+        }
+        /// <summary>
+        /// derives the request for retrieving a wallets adress from a wallet id response
+        /// </summary>
+        /// <param name="derivation">the wallet id rpc which to derive the request from</param>
+        /// <param name="generateNewAddress">specifies if a new random address should be generated for privacy purposes</param>
+        public GetNextAddress_RPC(WalletID_Response derivation, bool generateNewAddress)
+        {
+            this.wallet_id = derivation.wallet_id;
+            this.new_address = generateNewAddress;
+        }
+        /// <summary>
         /// the wallet number of the wallet in the chia client (eg. 2)
         /// </summary>
         public ulong wallet_id { get; set; }
         /// <summary>
         /// specifies if a new address should be generated or the last one used
         /// </summary>
-        public bool new_address { get; set; }
+        public bool new_address { get; set; } = false;
     }
 }
