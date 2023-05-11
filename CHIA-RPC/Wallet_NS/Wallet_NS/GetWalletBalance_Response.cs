fb0a6e20e301feebe1a9a1b6ab0e561b0f9d36ed
Julian B <bechti44@googlemail.com>
2023-05-11 11:45:09 +0200

    - updated documentation of WalletBalanceInfo

--- CHIA-RPC/Wallet_NS/Wallet_NS/GetWalletBalance_Response.cs
+++ CHIA-RPC/Wallet_NS/Wallet_NS/GetWalletBalance_Response.cs
@@ -16,10 +16,15 @@ namespace CHIA_RPC.Wallet_NS.Wallet_NS
         /// </summary>
         public WalletBalanceInfo wallet_balance { get; set; }
     }
+    /// <summary>
+    /// <see href="https://docs.chia.net/wallet-rpc#get_wallet_balance"/><br/><br/>
+    /// Uses:<br/><see cref="General_NS.WalletID_RPC"/>
+    /// </summary>
     public class WalletBalanceInfo
     {
         /// <summary>
-        /// the confirmed wallet balance, which represents the total balance available for spending, in mojo.
+        /// represents the total balance which is in the wallet *right now*, <br/>
+        /// in mojo.
         /// </summary>
         public ulong confirmed_wallet_balance { get; set; }
 
@@ -31,11 +36,23 @@ namespace CHIA_RPC.Wallet_NS.Wallet_NS
         /// <summary>
         /// The maximum amount of Chia that can be sent in a single transaction, in mojo.
         /// </summary>
+        /// <remarks>
+        /// `in a single transaction` is important because small dust coins might consume a lot of space in a transaction block.<br/>
+        /// -> max_send_amount equates to the largest transaction you can make with your balance that still fits in one transaction block.
+        /// </remarks>
         public ulong max_send_amount { get; set; }
 
         /// <summary>
-        /// The amount of pending change, which represents the unconfirmed balance that will be available for spending once confirmed, in mojo.
+        /// The amount of pending change, which representsthe balance change when all pending transactions are completed.<br/>
+        /// might also be related to the "locked" balance (?)
         /// </summary>
+        /// <remarks>
+        /// this might be confusing.<br/>
+        /// Lets say you have one coin with worth of 10 xch and you send 1 xch. The pending change is 10 xch. Why?<br/>
+        /// the 10xch coin is splitted in two:<br/>
+        /// - 1xch coin which you send out<br/>
+        /// - 9xch coin which you send to yourself
+        /// </remarks>
         public ulong pending_change { get; set; }
 
         /// <summary>
@@ -46,16 +63,28 @@ namespace CHIA_RPC.Wallet_NS.Wallet_NS
         /// <summary>
         /// The spendable balance, which represents the total balance available for spending without waiting for any pending transactions, in mojo.
         /// </summary>
+        /// <remarks>
+        /// it is recommended to use <see cref="max_send_amount"/> for calculations since this is the maximum value that can be sent in one transaction (fits one block) whereas
+        /// <see cref="spendable_balance"/> is the maximum amount free for spending (in multiple transactions)<br/>
+        /// This is related to small/dust coins which might fill one block
+        /// </remarks>
         public ulong spendable_balance { get; set; }
 
         /// <summary>
-        /// The unconfirmed wallet balance, which represents the total balance including both confirmed and unconfirmed transactions, in mojo.
+        /// represents the presumed total balance which is in the wallet, including unconfirmed transactions<br/>
+        /// in other words: <br/>
+        /// confirmed_wallet_balance + pending incoming transactions - pending outgoing transactions<br/><br/>
+        /// in mojo.
         /// </summary>
         public ulong unconfirmed_wallet_balance { get; set; }
 
         /// <summary>
         /// The count of unspent coins, which represent the number of individual coin records that contribute to the wallet's total balance.
         /// </summary>
+        /// <remarks>
+        /// this has nothing to do with cats/tokens/currencies it rather represents unit packets of a given currency. 
+        /// (as if you have many coins of one currency in your wallet)
+        /// </remarks>
         public ulong unspent_coin_count { get; set; }
 
         /// <summary>
