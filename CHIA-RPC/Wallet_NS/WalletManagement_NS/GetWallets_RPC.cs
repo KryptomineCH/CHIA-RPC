4f902191c73bb6036a3a04d968e40f480af8dd6f
Julian B <bechti44@googlemail.com>
2023-06-21 11:26:02 +0200

    added possibility to specify wallet type

--- CHIA-RPC/Wallet_NS/WalletManagement_NS/GetWallets_RPC.cs
+++ CHIA-RPC/Wallet_NS/WalletManagement_NS/GetWallets_RPC.cs
@@ -65,10 +65,10 @@ namespace CHIA_RPC.Wallet_NS.WalletManagement_NS
         /// </summary>
         /// <param name="include_data">Set to true to include all coin info for this wallet [Default: true]</param>
         /// <param name="type">The type of wallet to retrieve. If included, must be one of `cat_wallet`, `did_wallet`, `nft_wallet`, or `pool_wallet`</param>
-        public GetWallets_RPC(bool include_data, string? type = null)
+        public GetWallets_RPC(bool include_data, WalletType? type = null)
         {
             this.include_data = include_data;
-            this.type = type;
+            this.type = type.ToString();
         }
 
         /// <summary>
