ac450a9d6d7a9f812aaed7d2d0aebe12d8d3691d
Julian B <bechti44@googlemail.com>
2023-08-18 11:09:48 +0200

    added tests for transaction no memo

---
+++ CHIA-RPC-Tests/Objects_NS/Test_Transaction_NoMemo.cs
@@ -0,0 +1,24 @@
+﻿using CHIA_RPC.Objects_NS;
+using CHIA_RPC_Tests.Testhelpers;
+using System.Transactions;
+
+namespace CHIA_RPC_Tests.Objects_NS
+{
+    public class Test_Transaction_NoMemo_GetPrimaryCoin
+    {
+        private DirectoryInfo TestFolder = new DirectoryInfo(@"TestAssets\Transaction_NoMemo\Get_PrimaryCoin");
+
+        [Fact]
+        public void Test_CoinSerialisation()
+        {
+            FileInfo[] testFiles = TestFolder.GetFiles();
+            foreach (FileInfo testFile in testFiles)
+            {
+                Transaction_NoMemo transaction = Transaction_NoMemo.LoadObjectFromFile(testFile);
+                Coin primaryCoin = transaction.GetPrimaryCoin();
+                { }
+            }
+        }
+
+    }
+}
