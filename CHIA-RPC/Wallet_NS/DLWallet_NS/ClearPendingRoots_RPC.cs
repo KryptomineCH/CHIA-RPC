40298c8d7cb3a774f25c502bcdc359d05faf76e0
Julian B <bechti44@googlemail.com>
2023-10-13 22:41:43 +0200

    add clear_pending_roots endpoint according to documentation

---
+++ CHIA-RPC/Wallet_NS/DLWallet_NS/ClearPendingRoots_RPC.cs
@@ -0,0 +1,38 @@
+﻿using CHIA_RPC.HelperFunctions_NS;
+using CHIA_RPC.Wallet_NS.DLWallet_NS.DLWalletObjects_NS;
+
+namespace CHIA_RPC.Wallet_NS.DLWallet_NS
+{
+    /// <summary>
+    /// Class to represent the response from a clear_pending_roots RPC call.
+    /// Functionality: Clear pending roots that will not be published, associated data may not be recoverable.
+    /// </summary>
+    /// <remarks>
+    /// Usage: chia rpc data_layer clear_pending_roots [REQUEST]
+    /// </remarks>
+    public class ClearPendingRoots_Response : ResponseTemplate<ClearPendingRoots_Response>
+    {
+        /// <summary>
+        /// Represents the root data of the response.
+        /// </summary>
+        public RootInfo root { get; set; }
+
+    }
+
+    /// <summary>
+    /// Class to represent the clear_pending_roots RPC call.
+    /// Functionality: Clear pending roots that will not be published, associated data may not be recoverable.
+    /// https://docs.chia.net/datalayer-rpc#clear_pending_roots
+    /// </remarks>
+    public class ClearPendingRoots_RPC : RPCTemplate<ClearPendingRoots_RPC>
+    {
+        /// <summary>
+        /// The hexadecimal store ID.
+        /// </summary>
+        /// <remarks>
+        /// This parameter is required.
+        /// </remarks>
+        public string store_id { get; set; }
+    }
+
+}
---
+++ CHIA-RPC/Wallet_NS/DLWallet_NS/DLWalletObjects_NS/RootInfo.cs
@@ -0,0 +1,33 @@
+﻿
+using CHIA_RPC.HelperFunctions_NS;
+
+namespace CHIA_RPC.Wallet_NS.DLWallet_NS.DLWalletObjects_NS
+{
+    /// <summary>
+    /// Represents the detailed information of the root.
+    /// Derived from the base class ObjectTemplate<RootInfo>.
+    /// </summary>
+    public class RootInfo : ObjectTemplate<RootInfo>
+    {
+        /// <summary>
+        /// Represents the generation number of the root.
+        /// </summary>
+        public ulong generation { get; set; }
+
+        /// <summary>
+        /// Represents the node hash of the root.
+        /// </summary>
+        public string node_hash { get; set; }
+
+        /// <summary>
+        /// Represents the status of the root.
+        /// </summary>
+        public int status { get; set; }
+
+        /// <summary>
+        /// Represents the tree ID of the root, which is a hexadecimal store ID.
+        /// </summary>
+        public string tree_id { get; set; }
+    }
+
+}
