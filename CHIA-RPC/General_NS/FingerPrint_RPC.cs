e6acd83040e245c7582de9c8206db41e327f8543
Julian B <bechti44@googlemail.com>
2023-10-13 17:33:43 +0200

    more implicit conversions

--- CHIA-RPC/General_NS/FingerPrint_RPC.cs
+++ CHIA-RPC/General_NS/FingerPrint_RPC.cs
@@ -1,4 +1,6 @@
-﻿using CHIA_RPC.HelperFunctions_NS;
+﻿using CHIA_RPC.Daemon_NS.KeychainServer_NS;
+using CHIA_RPC.Daemon_NS.KeychainServer_NS.KeychainServer_NS;
+using CHIA_RPC.HelperFunctions_NS;
 
 namespace CHIA_RPC.General_NS
 {
@@ -17,6 +19,12 @@ namespace CHIA_RPC.General_NS
     /// <summary>
     /// used for a variety of wallet management and wallet selection tasks
     /// </summary>
+    /// <remarks>
+    /// an implicit conversion from <see cref="ulong"/> exists<br/>
+    /// an implicit conversion from <see cref="FingerPrint_Response"/> exists<br/>
+    /// an implicit conversion from <see cref="Key"/> exists<br/>
+    /// an implicit conversion from <see cref="GetKey_Response"/> exists<br/>
+    /// </remarks>
     public class FingerPrint_RPC : RPCTemplate<FingerPrint_RPC>
     {
         /// <summary>
@@ -39,6 +47,14 @@ namespace CHIA_RPC.General_NS
         {
             this.fingerprint = fingerprint.fingerprint;
         }
+        public FingerPrint_RPC(Key key)
+        {
+            this.fingerprint = key.fingerprint;
+        }
+        public FingerPrint_RPC(GetKey_Response keyResponse)
+        {
+            this.fingerprint = keyResponse.key.fingerprint;
+        }
 
         /// <summary>
         /// The wallet's fingerprint, obtainable by running chia wallet show
@@ -51,6 +67,85 @@ namespace CHIA_RPC.General_NS
         public static implicit operator FingerPrint_RPC(FingerPrint_Response response)
         {
             return new FingerPrint_RPC(response);
+        }
+        public static implicit operator FingerPrint_RPC(Key response)
+        {
+            return new FingerPrint_RPC(response);
+        }
+        public static implicit operator FingerPrint_RPC(GetKey_Response response)
+        {
+            return new FingerPrint_RPC(response);
+        }
+    }
+    /// <summary>
+    /// used for a variety of wallet management and wallet selection tasks
+    /// </summary>
+    /// <remarks>
+    /// an implicit conversion from <see cref="Key"/>[] exists<br/>
+    /// an implicit conversion from <see cref="GetKeys_Response"/> exists<br/>
+    /// an implicit conversion from <see cref="ulong"/>[] exists
+    /// </remarks>
+    public class FingerPrints_RPC : RPCTemplate<FingerPrints_RPC>
+    {
+        /// <summary>
+        /// parameterless constructor, for serializer
+        /// </summary>
+        public FingerPrints_RPC() { /* for serialisation */ }
+        /// <summary>
+        /// used for a variety of wallet management and wallet selection tasks
+        /// </summary>
+        /// <param name="fingerprint">The wallet's fingerprint, obtainable by running chia wallet show</param>
+        public FingerPrints_RPC(ulong[] fingerprints)
+        {
+            this.fingerprints = fingerprints;
+        }
+        public FingerPrints_RPC(Key[] keys)
+        {
+            this.fingerprints = new ulong[keys.Length]; 
+            for(int i = 0; i < this.fingerprints.Length; i++)
+            {
+                this.fingerprints[i] = keys[i].fingerprint;
+            }
+        }
+        public FingerPrints_RPC(GetKeys_Response keysResponse)
+        {
+            this.fingerprints = new ulong[keysResponse.keys.Length];
+            for (int i = 0; i < this.fingerprints.Length; i++)
+            {
+                this.fingerprints[i] = keysResponse.keys[i].fingerprint;
+            }
+        }
+
+        /// <summary>
+        /// used for a variety of wallet management and wallet selection tasks
+        /// </summary>
+        /// <param name="fingerprint">The wallet's fingerprint, obtainable by running chia wallet show</param>
+        public FingerPrints_RPC(FingerPrint_Response[] fingerprints)
+        {
+            List<ulong> ulongs = new List<ulong>();
+            foreach(FingerPrint_Response print in fingerprints) ulongs.Add(print.fingerprint);
+            this.fingerprints = ulongs.ToArray();
+        }
+
+        /// <summary>
+        /// The wallet's fingerprint, obtainable by running chia wallet show
+        /// </summary>
+        public ulong[] fingerprints { get; set; }
+        /// <summary>
+        /// Implicit conversion from FingerPrint_Response to DidIFingerPrint_RPCD_RPC.
+        /// </summary>
+        /// <param name="response">FingerPrint_RPC object</param>
+        public static implicit operator FingerPrints_RPC(FingerPrint_Response[] response)
+        {
+            return new FingerPrints_RPC(response);
+        }
+        public static implicit operator FingerPrints_RPC(Key[] response)
+        {
+            return new FingerPrints_RPC(response);
+        }
+        public static implicit operator FingerPrints_RPC(GetKeys_Response response)
+        {
+            return new FingerPrints_RPC(response);
         }
     }
 }
