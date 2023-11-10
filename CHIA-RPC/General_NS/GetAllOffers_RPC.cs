cc80a854c6d497a99181814dec7e0009950e6218
Julian B <bechti44@googlemail.com>
2023-11-10 16:18:29 +0100

    Updated GetAllOffers_RPC Documentation updated get_all_offers sort_key type (string)

--- CHIA-RPC/General_NS/GetAllOffers_RPC.cs
+++ CHIA-RPC/General_NS/GetAllOffers_RPC.cs
@@ -17,15 +17,17 @@ namespace CHIA_RPC.General_NS
         /// <summary>
         /// Class to represent the request for the "get_all_offers" RPC method.
         /// </summary>
-        /// <param name="start">The sequence number of the first offer to show. Default: 0</param>
-        /// <param name="end">The sequence number of the last offer to show. Default: 10</param>
+        /// <param name="start">The sequence number of the first offer to show. Default: 0; Max: long.MaxValue -1</param>
+        /// <param name="end">The sequence number of the last offer to show. Default: 10; Max: long.MaxValue</param>
         /// <param name="exclude_my_offers">Set to true to exclude offers you originated. Default: false</param>
         /// <param name="exclude_taken_offers">Set to true to exclude offers that have already been taken. Default: false</param>
         /// <param name="include_completed">Set to true to include offers that have been taken. Default: false</param>
         /// <param name="sort_key">Specify the key for sorting. Default: None</param>
-        /// <param name="reverse">Set to true to sort the results in reverse order. Default: false</param>
+        /// <param name="reverse">Set to true to sort the results in reverse order. Default: false<br/>
+        /// true sets the default sorting key from oldest to newest, ideal to build a history.<br/>
+        /// false lists from new to old, perfect to list the last X offers</param>
         /// <param name="file_contents">Set to true to display the contents of each offer. Default: false</param>
-        public GetAllOffers_RPC(ulong start, ulong end, bool? exclude_my_offers = null, bool? exclude_taken_offers = null, bool? include_completed = null, ulong? sort_key = null, bool? reverse = null, bool? file_contents = null)
+        public GetAllOffers_RPC(ulong start, ulong end, bool? exclude_my_offers = null, bool? exclude_taken_offers = null, bool? include_completed = null, string? sort_key = null, bool? reverse = null, bool? file_contents = null)
         {
             this.start = start;
             this.end = end;
@@ -40,11 +42,13 @@ namespace CHIA_RPC.General_NS
         /// <summary>
         /// The sequence number of the first offer to show. Default: 0
         /// </summary>
+        /// <remarks>Maximum is long.MaxValue -1!</remarks>
         public ulong start { get; set; } = 0;
 
         /// <summary>
         /// The sequence number of the last offer to show. Default: 10
         /// </summary>
+        /// <remarks>Maximum is long.MaxValue!</remarks>
         public ulong end { get; set; } = 10;
 
         /// <summary>
@@ -65,11 +69,13 @@ namespace CHIA_RPC.General_NS
         /// <summary>
         /// Specify the key for sorting. Default: None
         /// </summary>
-        public ulong? sort_key { get; set; }
+        public string? sort_key { get; set; }
 
         /// <summary>
         /// Set to true to sort the results in reverse order. Default: false
         /// </summary>
+        /// <remarks>true sets the default sorting key from oldest to newest, ideal to build a history.<br/>
+        /// false lists from new to old, perfect to list the last X offers</remarks>
         public bool? reverse { get; set; }
 
         /// <summary>
