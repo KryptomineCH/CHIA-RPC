b8c4c64924c24a4efe8235e012e648a6d7f98992
Julian Bechtold <julian.bechtold@outlook.com>
2023-11-28 00:05:06 +0100

    added offers chunk (incomplete)

--- CHIA-RPC/Objects_NS/OffersChunk.cs
+++ CHIA-RPC/Objects_NS/OffersChunk.cs
@@ -6,14 +6,14 @@ using System.Text.Json.Serialization;
 namespace CHIA_RPC.Objects_NS
 {
     /// <summary>
-    /// thread safe implementation of a transactions chunk file.<br/>
-    /// Default size: max 1000 Blocks (~1GB if all transactions were to be saved and filled)
+    /// thread safe implementation of a offers chunk file.<br/>
+    /// Default size: max 1000 Blocks (~1GB if all offers were to be saved and filled)
     /// </summary>
     /// <remarks>
     /// StartBlock should be 0 Based
     /// eg: 0-999, 1000-1999, 15000-15999
     /// </remarks>
-    public class TransactionsChunk : ObjectTemplate<TransactionsChunk>
+    public class OffersChunk : ObjectTemplate<OffersChunk>
     {
         /// <summary>
         /// The minimum Block height to be included in this chunk file
@@ -38,15 +38,15 @@ namespace CHIA_RPC.Objects_NS
         public ulong ChunkSize { get; set; } = 1000;
 
         /// <summary>
-        /// calculates the height of the latest transaction in this Blockfile
+        /// calculates the height of the latest offer in this Blockfile
         /// </summary>
         [JsonIgnore]
-        public ulong LastTransactionBlock
+        public ulong LastOfferBlock
         {
             get
             {
                 ulong height = 0;
-                foreach (ulong blockHeight in TransactionsByBlock.Keys)
+                foreach (ulong blockHeight in OffersByBlock.Keys)
                 {
                     if (blockHeight > height)
                         height = blockHeight;
@@ -63,57 +63,59 @@ namespace CHIA_RPC.Objects_NS
         public bool Edited { get; set; } = false;
 
         /// <summary>
-        /// holds all transactions in the chunkfile.
+        /// holds all offers in the chunkfile.
         /// </summary>
         /// <remarks>
-        /// Please add with <see cref="AddTransaction"/>, except you want unsafe handling for added performance
+        /// Please add with <see cref="AddOffer"/>, except you want unsafe handling for added performance
         /// </remarks>
-        public ConcurrentDictionary<ulong, ConcurrentBag<Transaction_DictMemos>> TransactionsByBlock { get; set; } = new ();
+        public ConcurrentDictionary<ulong, ConcurrentBag<OfferFile>> OffersByBlock { get; set; } = new ();
         
         /// <summary>
-        /// Adds a transaction to the Chunkfile
+        /// Adds an offer to the Chunkfile
         /// </summary>
         /// <remarks>
-        /// verifies the transaction is Confirmed and the height was found
+        /// verifies the offer is Confirmed and the height was found
         /// </remarks>
-        /// <param name="transaction">the transaction to add</param>
+        /// <param name="offer">the transaction to add</param>
         /// <exception cref="InvalidOperationException">transaction is not confirmed or height unclear</exception>
-        public void AddTransaction(Transaction_DictMemos transaction, bool checkIfConfirmed)
+        public void AddTransaction(OfferFile offer, bool checkIfConfirmed)
         {
             // pre-add checks
-            if (checkIfConfirmed && !(bool)transaction.confirmed)
+            if (checkIfConfirmed && offer.trade_record.accepted_at_time != null && offer.trade_record.accepted_at_time != 0)
             {
                 throw new InvalidOperationException("the transaction is not in status confirmed!"); 
             }
-            if (transaction.confirmed_at_height == null)
+            if (offer.trade_record.confirmed_at_index == null)
             {
                 throw new InvalidOperationException("the block of the transaction could not be determined! confirmed_at_height == null");
             }
             // get and check height
-            ulong height = transaction.confirmed_at_height.Value;
+            ulong height = offer.trade_record.confirmed_at_index.Value;
             if (height < StartBlock || height > EndBlock)
             {
                 throw new InvalidOperationException($"the transaction is at height {height} but the bundle range is {StartBlock}-{EndBlock}");
             }
             // add if block already exists
-            if (TransactionsByBlock.TryGetValue(height, out ConcurrentBag<Transaction_DictMemos> blockList))
+            if (OffersByBlock.TryGetValue(height, out ConcurrentBag<OfferFile> blockList))
             {
                 bool isContained = false;
-                foreach (Transaction_DictMemos transactionToCompare in blockList)
+                foreach (OfferFile offerToCompare in blockList)
                 {
-                    if (transaction == transactionToCompare)
+                    throw new NotImplementedException("offer comparison not yet implemented!");
+                    if (offer == offerToCompare)
                     {
+                        
                         isContained = true;
                         break;
                     }
                 }
                 if(!isContained)
-                    blockList.Add(transaction);
+                    blockList.Add(offer);
             }
             // create block if not exists
             else
             {
-                TransactionsByBlock[height] = new ConcurrentBag<Transaction_DictMemos> { transaction };
+                OffersByBlock[height] = new ConcurrentBag<OfferFile> { offer };
             }
             Edited = true;
         }
